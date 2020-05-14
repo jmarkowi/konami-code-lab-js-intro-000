15d263c2f2ebaba6dcfb00dc8bb3713c05260a40
Jesse Markowitz <jess.markowitz@gmail.com>
2020-05-14 02:49:00 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,44 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+//let counter = 0;
+
+//function init() {
+//  document.body.addEventListener("keydown", (event) => {
+  //  const key = event.key;
+//check the key against the Konami Code array
+//if it matches, increase counter and listen for next keystroke
+    //if (key === codes[counter]) {
+      //counter++;
+//the only way for counter to equal 10 is if all keystrokes have been correct!
+      //if (counter === codes.length) {
+        //alert("Congratulations! You have cracked the code!");
+      //  counter = 0;
+      //}
+//if it doesn't match, reset the counter and listen for the
+    //} else {
+      //counter = 0;
+  //  }
+  //});
+//}
+
+//Alternative solution: add keystrokes to an array, then check the array against the codes array
+
+let temp = []
+
+function initAlt() {
+  document.body.addEventListener("keydown", (event) => {
+    debugger
+    temp.push(event.key);
+    if (temp === codes) {
+      alert("Congratulations! You have cracked the code!");
+    } else {
+        for (let i = 0; i < temp.length; i++) {
+            if (temp[i] !== codes[i]) {
+                temp = []
+                break
+            }
+        }
+      }
+  })
 }
